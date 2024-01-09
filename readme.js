489f04f1fe2a7a675b46e1ca126a0e8641195405
Shado2107 <romalmeida21@gmail.com>
2024-01-09 23:06:12 +0000

    update readme

---
+++ readme.js
@@ -0,0 +1,40 @@
+module.exports = ` <h3 align="center">Hey there, I'm <a href="https://aitalmeida.tech/">Pavel</a> - aka Shado <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="28"> <img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="28"/></h3>
+
+
+## 📖 About me
+
+* 💻 I'm a web developer
+* 🎓 I'm a Computer Science student 
+* 🌐 You can see some of my projects on [my portfolio](https://aitalmeida.tech/)
+  
+
+
+## 🙋‍♂️ Connect with me:
+
+* whatsapp : [+228 99 75 23 27](https://wa.me/22899752327)
+* Twitter : [Almeida Pavel](https://twitter.com/AlmeidaPavel)
+* Facebook :
+
+
+## 👨‍💻 Languages and Tools:
+
+* ReactJs 
+* Nextjs
+* Javascript
+* Php
+* Laravel
+
+##
+
+[![](https://visitcount.itsvg.in/api?id=Shado2107&label=Profile%20Views&color=1&icon=0&pretty=true)](https://visitcount.itsvg.in)
+
+
+![Pavel's GitHub stats](https://github-readme-stats.vercel.app/api?username=Shado2107&show_icons=true&theme=dark)
+
+
+</details>
+
+Credits: [Shado2107](https://github.com/Shado2107)
+
+Last Edited on: 22/04/2021
+ `;
